88071eb4f8ad31321ccb204ce237fce78fc4e3b0
Isiah Reyes <i0576631@amarillocollege.com>
2025-03-17 17:03:49 -0500

    third commit

---
+++ script.js
@@ -0,0 +1,13 @@
+//display alert 
+window.alert("Good Afternoon");
+ //declare the variable to hold the div element
+let container = document.getElementByID("container");
+//getElementByID is he method to locate the element by ID
+//container is an HTML element, it is a div in this case
+//set the content of the element
+
+container.innerHTML = "This is the container place";
+//change the color style into red
+container.style.color = "red";
+//add the new element p into the page
+document.write("<p>This is a new paragraph</p>");
